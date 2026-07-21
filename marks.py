ca0734348bb9a52a9ac4af61ad88294fcb4bebc6
random <random@gmail.com>
2026-07-21 19:17:40 +0530

    change 3 in marks.py

--- marks.py
+++ marks.py
@@ -2,3 +2,6 @@ print("This is my marks.py")
 print("student a marks for social -85")
 
 print("Trying to edit marks from another branch with the prev work in stash")
+print("change 1")
+print("change 2")
+print("change 3")
